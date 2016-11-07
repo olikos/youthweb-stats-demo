76001c2001378b56520f850d5ca2dea42fd4ff1f
Artur Weigandt <Art4@users.noreply.github.com>
2016-11-08 00:23:43 +0100

    Update Url zur Youthweb-API

--- app/src/main/java/local/youthweb/youthwebstats/ParamsYouthwebStats.java
+++ app/src/main/java/local/youthweb/youthwebstats/ParamsYouthwebStats.java
@@ -7,8 +7,8 @@ public class ParamsYouthwebStats {
     public static final String ACCOUNT = "Accounts";
     public static final String FORUM = "Forum";
     public static final String GROUPS = "Groups";
-    public static final String ACCOUNT_URL = "https://youthweb.net/stats/account";
-    public static final String FORUM_URL = "https://youthweb.net/stats/forum";
-    public static final String GROUPS_URL = "https://youthweb.net/stats/groups";
+    public static final String ACCOUNT_URL = "https://api.youthweb.net/stats/account";
+    public static final String FORUM_URL = "https://api.youthweb.net/stats/forum";
+    public static final String GROUPS_URL = "https://api.youthweb.net/stats/groups";
 }
 
